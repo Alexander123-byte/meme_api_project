374d84398524358d8222a49c2335cf4bdaccad86
Alexander <san.juckov2017@yandex.ru>
2024-06-21 19:07:56 +0300

    Создание схем

---
+++ app/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class MemeBase(BaseModel):
+    title: str
+
+
+class MemeCreate(MemeBase):
+    pass
+
+
+class MemeUpdate(MemeBase):
+    pass
+
+
+class Meme(MemeBase):
+    id: int
+    image_url: str
+
+    class Config:
+        orm_mode = True
